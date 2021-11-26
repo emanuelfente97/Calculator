98a3f0160cd4fa716f595fb75b96b26da67f7fe2
emanuelfente97 <emanuelfente97@gmail.com>
2021-11-26 15:21:57 +0200

    first commit

---
+++ myscipt.js
@@ -0,0 +1,13 @@
+
+let sum="";
+function addToSum(operator){
+    sum = sum + operator;
+    document.getElementById("output").innerText =sum;
+}
+function evaluateSum(){
+    document.getElementById("output").innerText = eval(sum);
+}
+function clearSum() {
+    sum = "";
+    document.getElementById("output").innerText = sum;
+}
